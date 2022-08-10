a345cb7ca67217d081852d0fe30e279bc82ed474
Ju, Minji <alswl0w0@gmail.com>
2022-08-10 13:14:01 +0900

    feat : Question 클래스에 OneToMany

--- src/main/java/com/ll/exam/sbb/Question.java
+++ src/main/java/com/ll/exam/sbb/Question.java
@@ -20,4 +20,7 @@ public class Question {
     @Column(columnDefinition = "TEXT")
     private String content;
     private LocalDateTime createDate;
+
+    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+    private List<Answer> answerList; // DB 컬럼에는 생성X
 }

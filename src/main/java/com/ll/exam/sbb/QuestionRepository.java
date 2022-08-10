0fb804b8b40c476d4beb5573b503c478d338e895
Ju, Minji <alswl0w0@gmail.com>
2022-08-10 16:05:34 +0900

    test : like 연산자 테스트케이스 추가

--- src/main/java/com/ll/exam/sbb/QuestionRepository.java
+++ src/main/java/com/ll/exam/sbb/QuestionRepository.java
@@ -2,8 +2,11 @@ package com.ll.exam.sbb;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Integer> { // Integer는 Primary Key
     Question findBySubject(String subject);
 
     Question findBySubjectAndContent(String subject, String content);
+    List<Question> findBySubjectLike(String s); // 여러개 반환될 수도 있으므로 리턴타입을 List로
 }

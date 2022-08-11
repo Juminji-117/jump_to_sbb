9550ba636171b6074649ce3c12d3bc533a97ff85
Ju, Minji <alswl0w0@gmail.com>
2022-08-11 13:37:57 +0900

    test : question truncate 테스트케이스 추가

--- src/main/java/com/ll/exam/sbb/QuestionRepository.java
+++ src/main/java/com/ll/exam/sbb/QuestionRepository.java
@@ -2,6 +2,10 @@ package com.ll.exam.sbb;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
 import java.util.List;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> { // Integer는 Primary Key
@@ -9,4 +13,9 @@ public interface QuestionRepository extends JpaRepository<Question, Integer> { /
 
     Question findBySubjectAndContent(String subject, String content);
     List<Question> findBySubjectLike(String s); // 여러개 반환될 수도 있으므로 리턴타입을 List로
+    @Transactional
+    @Modifying
+    @Query(value = "truncate question", nativeQuery = true)
+    void truncate();
 }
+

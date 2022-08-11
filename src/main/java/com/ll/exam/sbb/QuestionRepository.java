67f988d8e24be443200720b91fa170cbe8bc696f
Ju, Minji <alswl0w0@gmail.com>
2022-08-11 16:38:10 +0900

    test&refactor : 인터페이스 상속 이용하여 코드중복 제거

--- src/main/java/com/ll/exam/sbb/QuestionRepository.java
+++ src/main/java/com/ll/exam/sbb/QuestionRepository.java
@@ -1,31 +1,23 @@
 package com.ll.exam.sbb;
 
+import com.ll.exam.sbb.base.RepositoryUtil;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
-public interface QuestionRepository extends JpaRepository<Question, Integer> { // Integer는 Primary Key
+public interface QuestionRepository extends JpaRepository<Question, Integer>, RepositoryUtil { // Integer는 Primary Key
     Question findBySubject(String subject);
 
     Question findBySubjectAndContent(String subject, String content);
+
     List<Question> findBySubjectLike(String s); // 여러개 반환될 수도 있으므로 리턴타입을 List로
+
     @Transactional
     @Modifying
     @Query(value = "truncate question", nativeQuery = true)
     void truncate();
-
-    @Transactional
-    @Modifying
-    @Query(value = "SET FOREIGN_KEY_CHECKS = 0", nativeQuery = true)
-    void disableForeignKeyChecks();
-
-    @Transactional
-    @Modifying
-    @Query(value = "SET FOREIGN_KEY_CHECKS = 1", nativeQuery = true)
-    void enableForeignKeyChecks();
 }
 

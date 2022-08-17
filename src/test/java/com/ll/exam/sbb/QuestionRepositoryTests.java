99385bd62969d47669914ef5e0be5c0da9600245
Ju, Minji <alswl0w0@gmail.com>
2022-08-17 09:36:28 +0900

    style : 부트스트랩, 지마켓산스폰트 도입

--- src/test/java/com/ll/exam/sbb/QuestionRepositoryTests.java
+++ src/test/java/com/ll/exam/sbb/QuestionRepositoryTests.java
@@ -13,7 +13,7 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
-public class QuestionRepositoryTests { // QustionRepositoryTests ver.instructor
+public class QuestionRepositoryTests {
     @Autowired
     private QuestionRepository questionRepository;
     private static int lastSampleDataId;

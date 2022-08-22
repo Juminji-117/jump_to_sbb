d1e3e226e35c536b98c4eae31e93b95a3eff6240
Ju, Minji <alswl0w0@gmail.com>
2022-08-22 13:13:05 +0900

    feat : 답변 작성시 현재 로그인한 회원을 작성자로 지정

--- src/main/java/com/ll/exam/sbb/answer/AnswerService.java
+++ src/main/java/com/ll/exam/sbb/answer/AnswerService.java
@@ -1,6 +1,7 @@
 package com.ll.exam.sbb.answer;
 
 import com.ll.exam.sbb.question.Question;
+import com.ll.exam.sbb.user.SiteUser;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,11 +12,13 @@ import java.time.LocalDateTime;
 public class AnswerService {
     private final AnswerRepository answerRepository;
 
-    public void create(Question question, String content) {
+    public void create(Question question, String content, SiteUser author) {
         Answer answer = new Answer();
         answer.setContent(content);
         answer.setCreateDate(LocalDateTime.now());
+        answer.setAuthor(author);
         question.addAnswer(answer); // OneToMany의 단방향 문제 해결하기 위해 answer.setQuestion 대신 바로 question 객체에 저장하는 것이 좋음
+
         answerRepository.save(answer);
     }
 }

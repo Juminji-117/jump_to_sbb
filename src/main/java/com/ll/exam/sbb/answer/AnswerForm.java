e6c56c9ce1d05c370aa2383f29c4857b7e2a1b0b
Ju, Minji <alswl0w0@gmail.com>
2022-08-23 15:35:55 +0900

    feat : 답변 수정 화면

--- src/main/java/com/ll/exam/sbb/answer/AnswerForm.java
+++ src/main/java/com/ll/exam/sbb/answer/AnswerForm.java
@@ -1,11 +1,12 @@
 package com.ll.exam.sbb.answer;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.Setter;
 
 import javax.validation.constraints.NotEmpty;
 
-@AllArgsConstructor
+// AnswerController.modify()에서 answerForm.setContent 필요
+@Setter
 @Getter
 public class AnswerForm {
     @NotEmpty(message = "내용은 필수항목입니다.")

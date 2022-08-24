60eb05190bd71437004e22b301d4435bd5c5dc29
Ju, Minji <alswl0w0@gmail.com>
2022-08-24 17:39:14 +0900

    feat : kw 파라미터가 질문제목 + 질문내용 + 질문작성자명 + 답변내용 검색어로 작동, 중복제거
    *Distinct 키워드 이용하여 답변(게시글 입장에서 OneToMany->게시글 조회기능이므로 답변이 여러개면 게시글 하나가 여러개로 중복노출) 중복제거

--- src/main/java/com/ll/exam/sbb/question/QuestionRepository.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionRepository.java
@@ -26,6 +26,8 @@ public interface QuestionRepository extends JpaRepository<Question, Long>, Repos
     //Question 입장에서 Answer 엔티티는 AnswerList -> AnswerList_content == Answer객체의 content
     Page<Question> findBySubjectContainsOrContentContainsOrAuthor_usernameContainsOrAnswerList_contentContains(String kw, String kw_, String kw__, String kw___, Pageable pageable);
 
+    Page<Question> findDistinctBySubjectContainsOrContentContainsOrAuthor_usernameContainsOrAnswerList_contentContains(String kw, String kw_, String kw__, String kw___, Pageable pageable);
+
     @Transactional
     @Modifying
     @Query(value = "ALTER TABLE question AUTO_INCREMENT = 1", nativeQuery = true)

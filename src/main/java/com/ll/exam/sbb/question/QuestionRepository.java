11ea923c9085b389602bcddd43c5b66287c0d3ff
Ju, Minji <alswl0w0@gmail.com>
2022-08-24 17:36:27 +0900

    feat : kw 파라미터가 질문제목 + 질문내용 + 질문작성자명 + 답변내용 검색어로 작동

--- src/main/java/com/ll/exam/sbb/question/QuestionRepository.java
+++ src/main/java/com/ll/exam/sbb/question/QuestionRepository.java
@@ -22,6 +22,10 @@ public interface QuestionRepository extends JpaRepository<Question, Long>, Repos
     Page<Question>findBySubjectContainsOrContentContains(String kw, String kw_, Pageable pageable);
 
     Page<Question>findBySubjectContainsOrContentContainsOrAuthor_usernameContains(String kw, String kw_, String kw__,Pageable pageable);
+
+    //Question 입장에서 Answer 엔티티는 AnswerList -> AnswerList_content == Answer객체의 content
+    Page<Question> findBySubjectContainsOrContentContainsOrAuthor_usernameContainsOrAnswerList_contentContains(String kw, String kw_, String kw__, String kw___, Pageable pageable);
+
     @Transactional
     @Modifying
     @Query(value = "ALTER TABLE question AUTO_INCREMENT = 1", nativeQuery = true)
